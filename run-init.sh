631f30dc46f5f7bd5a84203fdee545bcd2670842
Marcos Ferreira <ferreira.mr@outlook.com>
2024-11-13 14:49:57 -0300

    feature: Adicionado phpMyAdmin

--- run-init.sh
+++ run-init.sh
@@ -4,6 +4,6 @@ echo "Running init-db.sh to generate init-db.sql..."
 ./init-db.sh
 
 echo "Starting containers with Docker Compose..."
-docker-compose -p webserver  up -d
+docker-compose -p webserver up -d
 
 echo "Success: Containers are up and running."
